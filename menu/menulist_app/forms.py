53944dcc4b6979eacda6e4715618ff668b24a677
adowmandek <adowmandek@gmail.com>
2021-11-03 07:10:17 +0300

    First commit

--- menu/menulist_app/forms.py
+++ menu/menulist_app/forms.py
@@ -1,7 +1,5 @@
 
 from django import forms
-from django.db.models.base import Model
-from django.forms import fields
 from.models import Menu
 
 class Menuregistrationform(forms.ModelForm):
--- menu/menulist_app/views.py
+++ menu/menulist_app/views.py
@@ -1,15 +1,30 @@
 # from Menu.models import Menu
+from django.http.response import HttpResponseRedirect
 from menulist_app.models import Menu
 from django.shortcuts import render
-from django.forms import Form
-from django.urls.conf import re_path
 from django.shortcuts import redirect, render
-from django.http.response import HttpResponse
-from django.db.models.aggregates import StdDev
 from .forms import Menuregistrationform
 
 
 
+def menu_register(request):
+     form=Menuregistrationform
+     return render(request,"menu_register.html",{"form":form})
+
+
+def menu_register(request):
+     if request.method=="POST":
+          form=Menuregistrationform(request.POST, request.FILES)
+          if form.is_valid():
+               form.save()
+               
+     else:
+          print("form.error")
+
+          form=Menuregistrationform()
+     return render(request,"menu_register.html",{"form":form})
+
+   
 
 def menu_list(request):
      menu= Menu.objects.all()
@@ -19,29 +34,18 @@ def menu_list(request):
 def  edit_menu(request,id):
      menu=Menu.objects.get(id=id)
 
-     # return redirect(request,"edit_menu.html",)
-
      if request.method=="POST":
           form=Menuregistrationform(request.POST,instance=menu)
           if form.is_valid():
                form.save()
      else:
           form=Menuregistrationform(instance=menu)
-          return render(request,"edit_menu.html",{"form":form})
-     return redirect(request,"menu_register.html",)
+          return render(request,"edit_menu.html", {"form":form})
+     # return redirect(request,"menu_register.html",)
 
 
 def delete_menu(request,id):
           menu=Menu.objects.get(id=id)
           menu.delete()
-          return redirect("menu_list")
-def menu_register(request):
-     if request.method=="POST":
-          form=Menuregistrationform(request.POST)
-          if form.is_valid():
-               form.saved()
-     # else:
-     #     print("form.error")
+          return redirect(request, "menu_list")
 
-     form=Menuregistrationform()
-     return render(request,"menu_register.html",{"form":form})
